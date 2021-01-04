4b4d94b0c8e028f0ea5fbc62356dca064a964ad0
Scott Lai <tuonglai3602@gmail.com>
2021-01-03 23:01:06 -0800

    Added more controls.

--- src/components/CustomBar.tsx
+++ src/components/CustomBar.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 interface CustomBarProp {
   isSorting: boolean;
@@ -7,17 +8,40 @@ interface CustomBarProp {
 }
 
 const CustomBar = ({ resizeBars, isSorting, startSorting }: CustomBarProp) => {
+  const [numberOfBars, setNumberOfBars] = useState(8);
   return (
-    <div>
-      <input
-        type='range'
-        min={8}
-        max={64}
-        value={8}
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+      }}
+    >
+      <div>
+        <Form.Label>Size: {numberOfBars}</Form.Label>
+        <Form.Control
+          type='range'
+          min={8}
+          max={64}
+          step={2}
+          value={numberOfBars}
+          disabled={isSorting}
+          onChange={({ target: { value } }) => {
+            const newNumberOfBars = Number(value);
+            setNumberOfBars(newNumberOfBars);
+            resizeBars(newNumberOfBars);
+          }}
+        />
+      </div>
+      <Button onClick={startSorting} disabled={isSorting}>
+        Sort!
+      </Button>
+      <Button
+        variant='secondary'
+        onClick={() => resizeBars(numberOfBars)}
         disabled={isSorting}
-        onChange={({ target: { value } }) => resizeBars(Number(value))}
-      />
-      <button onClick={startSorting}>Sort!</button>
+      >
+        Regenerate
+      </Button>
     </div>
   );
 };
