976acb530090e3c48ae0d3d809d8a936e35d9dde
Scott Lai <tuonglai3602@gmail.com>
2021-01-04 17:01:15 -0800

    Removed unused imports.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import SortingAnimation from './components/SortingAnimation';
 import 'bootstrap/dist/css/bootstrap.min.css';
